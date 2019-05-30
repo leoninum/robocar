4d168687d44c1da87046dbaac58adf1eb8089aa2
helams <36235422+helams@users.noreply.github.com>
2019-05-30 11:17:31 +0200

    Update Pins.hpp

--- Pins.hpp
+++ Pins.hpp
@@ -9,6 +9,25 @@ Bluetooth, 2Pins an 0,1 oder SoftwareSerial(12, 13)
 L298 Motor Steuerung - MA, MB EnableA:7, EnableB:6, In1:5, In2:4, In3:3, In4:2
 LCD (I2c) - A5, A4
 Linienverfolgung - 3 Pins: A0,A1,A2
+
+Anschluss Motorregler (von hinten)
+  rot  schwarz  weiß  lila  blau  grün  gelb  orange
+   +     GND    ENB    IN4   IN3   IN2   IN1  ENA     Motorreglerplatine
+                  6     2     3     4     5    7      Arduino
+                  
+Linienfolger  schwarz  weiß  grau   blau  grün  gelb   braun  rot  orange
+                GND     +     S      GND   +     S      GND    +      S      Platine
+                             A0                  A1                  A2      Arduino
+                             
+LCD    GND   VCC    SDA   SCL       
+       grau  rot    grün  orange
+       
+Ultraschall    weiß   blau   lila   braun
+                VCC    TRIG   ECHO   GND        Platine
+                         10    9                Arduino
+ 
+ Infrarot      GND      VCC   Signal
+               schwarz  grau  lila
  */
 #define pHC05_ECHO 9
 #define pHC05_TRIGGER 10
